d571395efabd3bce07974cfdcbb52d3ea1d9af31
Irene <irenebenigna@gmail.com>
2021-01-25 00:32:55 -0600

    Update Sesion4_Postwork.R

--- Postworks/Sesion04/Postworks/Sesion4_Postwork.R
+++ Postworks/Sesion04/Postworks/Sesion4_Postwork.R
@@ -4,7 +4,7 @@ library(ggplot2)
 setwd("C:/Users/BALAMLAPTOP2/Documents/GitHub/data-analysis-santander/Sesion04/Postworks/")
 
 # Actividad 1
-# Importa los datos de soccer de la temporada 2019/2020 de la primera división de la liga española a R, 
+# Importa los datos de soccer de la temporada 2019/2020 de la primera divisiÃ³n de la liga espaÃ±ola a R, 
 # los datos los puedes encontrar en el siguiente enlace: https://www.football-data.co.uk/spainm.php
 # football <- read.csv("SP1.csv")
 u1718 <- "https://www.football-data.co.uk/mmz4281/1718/SP1.csv"
@@ -20,15 +20,15 @@ download.file(url = u1920, destfile = "SP1-1920.csv", mode = "wb")
 rawdata <- lapply(list.files(pattern = "*.csv"), read.csv)
 
 
-# Con la función select del paquete dplyr selecciona únicamente las columnas Date, HomeTeam, AwayTeam, FTHG, FTAG y FTR; 
-# esto para cada uno de los data frames. (Hint: también puedes usar lapply).
+# Con la funciÃ³n select del paquete dplyr selecciona Ãºnicamente las columnas Date, HomeTeam, AwayTeam, FTHG, FTAG y FTR; 
+# esto para cada uno de los data frames. (Hint: tambiÃ©n puedes usar lapply).
 
 selecteddata <- lapply(rawdata, select, Date, HomeTeam:FTR)
 
 
-# Asegúrate de que los elementos de las columnas correspondientes de los nuevos data frames sean del mismo tipo (Hint 1: usa as.Date 
-# y mutate para arreglar las fechas). Con ayuda de la función rbind forma un único data frame que contenga las seis columnas mencionadas 
-# en el punto 3 (Hint 2: la función do.call podría ser utilizada).
+# AsegÃºrate de que los elementos de las columnas correspondientes de los nuevos data frames sean del mismo tipo (Hint 1: usa as.Date 
+# y mutate para arreglar las fechas). Con ayuda de la funciÃ³n rbind forma un Ãºnico data frame que contenga las seis columnas mencionadas 
+# en el punto 3 (Hint 2: la funciÃ³n do.call podrÃ­a ser utilizada).
 mutateddata <- lapply(selecteddata, mutate, 
                       Date = as.Date(Date, "%d/%m/%y"),
                       HomeTeam = as.factor(HomeTeam),
@@ -41,14 +41,14 @@ football <- do.call(rbind, mutateddata)
 
 
 # Actividad 2
-# Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números de goles 
+# Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los nÃºmeros de goles 
 # anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
 
 # FTHG = Full Time Home Team Goals
 # FTAG = Full Time Away Team Goals
 
 # Actividad 3
-# Consulta cómo funciona la función table en R al ejecutar en la consola ?table
+# Consulta cÃ³mo funciona la funciÃ³n table en R al ejecutar en la consola ?table
 
 # Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
 
@@ -57,8 +57,8 @@ football <- do.call(rbind, mutateddata)
 #La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
 
 # Calculo de frecuencia relativa por variable
-# La frecuencia relativa es una medida estadística que se calcula como el cociente de la frecuencia absoluta de algún valor de la población/muestra (fi) 
-# entre el total de valores que componen la población/muestra (N).
+# La frecuencia relativa es una medida estadÃ­stica que se calcula como el cociente de la frecuencia absoluta de algÃºn valor de la poblaciÃ³n/muestra (fi) 
+# entre el total de valores que componen la poblaciÃ³n/muestra (N).
 
 # Frecuencia relativa del equipo local
 pm.fthg <- table(football$FTHG)/length(football$FTHG)
@@ -113,12 +113,12 @@ for (i in 1:length(pm.ftag)){
 }
 
 # Postwork sesion 4
-# Ahora investigarás la dependencia o independencia del número de goles anotados por el equipo de casa 
-# y el número de goles anotados por el equipo visitante mediante un procedimiento denominado bootstrap, 
-# revisa bibliografía en internet para que tengas nociones de este desarrollo.
+# Ahora investigarÃ¡s la dependencia o independencia del nÃºmero de goles anotados por el equipo de casa 
+# y el nÃºmero de goles anotados por el equipo visitante mediante un procedimiento denominado bootstrap, 
+# revisa bibliografÃ­a en internet para que tengas nociones de este desarrollo.
 
 # Ya hemos estimado las probabilidades conjuntas de que el equipo de casa anote X=x goles (x=0,1,... ,8), 
-# y el equipo visitante anote Y=y goles (y=0,1,... ,6), en un partido. Obtén una tabla de cocientes al 
+# y el equipo visitante anote Y=y goles (y=0,1,... ,6), en un partido. ObtÃ©n una tabla de cocientes al 
 # dividir estas probabilidades conjuntas por el producto de las probabilidades marginales correspondientes.
 df_cocientes <- table(data)/nrow(data)
 for (i in 1:dim(df_cocientes)[1]){
@@ -128,6 +128,10 @@ for (i in 1:dim(df_cocientes)[1]){
   }
 }
 
+# Mediante un procedimiento de boostrap, obtÃ©n mÃ¡s cocientes similares a los obtenidos en la tabla del punto 
+# anterior. Esto para tener una idea de las distribuciones de la cual vienen los cocientes en la tabla anterior. 
+# Menciona en cuÃ¡les casos le parece razonable suponer que los cocientes de la tabla en el punto 1, son iguales 
+# a 1 (en tal caso tendrÃ­amos independencia de las variables aleatorias X y Y).
 
 B = 999
 football.boot = vector(mode="double")
@@ -151,48 +155,3 @@ hist(football.boot, col="slateblue1")
 qqnorm(football.boot)
 qqline(football.boot)
 par(mfrow=c(1,1))
-
-
-# Mediante un procedimiento de boostrap, obtén más cocientes similares a los obtenidos en la tabla del punto 
-# anterior. Esto para tener una idea de las distribuciones de la cual vienen los cocientes en la tabla anterior. 
-# Menciona en cuáles casos le parece razonable suponer que los cocientes de la tabla en el punto 1, son iguales 
-# a 1 (en tal caso tendríamos independencia de las variables aleatorias X y Y).
-
-# asdf <- as.data.frame(df_cocientes)
-# freq.mean = with(asdf, mean(Freq))
-# freq.mean
-# 
-# B = 5000
-# n = nrow(asdf)
-# boot.samples = matrix(sample(asdf$Freq, size = B * n, replace = TRUE), B, n)
-# boot.statistics = apply(boot.samples, 1, mean)
-# 
-# ggplot(data.frame(Cociente = football.boot),aes(x=Cociente)) +
-#   geom_histogram(binwidth=0.25,aes(y=..density..)) +
-#   geom_density(color="red")
-# 
-# 
-# library(dispRity)
-# library(boot)
-# 
-# foo <- function(data, indices, cor.type){
-#   dt<-data[indices,]
-#   c(
-#     cor(dt[,1], dt[,2], method=cor.type),
-#     median(dt[,1]),
-#     median(dt[,2])
-#   )
-# }
-# 
-# fc <- function(d, i){
-#   d2 <- d[i,]
-#   return(cor(d2$write, d2$math))
-# }
-# 
-# set.seed(12345)
-# bootcorr <- boot(as.data.frame(df_cocientes), fc, R=500)
-# myBootstrap <- boot(as.data.frame(df_cocientes), foo, R=1000, cor.type='s')
-# 
-# xbarstar <- sapply(resamps, mean, simplify = TRUE)
-# srs <- rnorm(25, mean = 3)
-# results <- boot.matrix(as.data.frame(df_cocientes), bootstraps = 20)
