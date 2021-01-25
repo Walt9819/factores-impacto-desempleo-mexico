8a552660e53a4413ec3390411aefb14a53f38674
Walt9819 <Walt9819@users.noreply.github.com>
2021-01-25 17:57:28 -0600

    Create main.R

---
+++ Project/main.R
@@ -0,0 +1,39 @@
+## Main file ##
+
+#### Lectura de datos ####
+## Conexión a MongoDB
+
+## Lectura datos (DENUE, ENOE y COVID)
+# Lectura DENUE y COVID
+
+# Lectura ENOE (opcional)
+
+## Limpieza de datos
+# Seleccionar columnas deseadas
+# Transformar datos
+# Limpieza general
+
+#### Exploración de datos ####
+## Gráfico `pairs` pairs(iris[,1:4], pch = 19, lower.panel = NULL)
+
+## Heatmap correlaciones atributo vs atributo
+
+#### Análisis ####
+## Modelo "lineal" ENUE para predecir: probabilidad desempleo ~ c(sexo, edad, nivel educativo, sector económico, estado de residencia)
+
+## Modelo "Logit" ENUE para predecir: probabilidad desempleo ~ c(sexo, edad, nivel educativo, sector económico, estado de residencia)
+
+# Primer trimestre (sin restricciones)
+# Segundo y tercer trimestre (con restricciones por COVID)
+
+## Random forest
+## Modelo Bayesiano (opcional)
+
+## (Pensamiento al aire) Comparación entre variables antes del COVID (restricciones) y con COVID
+
+#### Dashboard ####
+## Indicadores propuestos por municipio (en mapa) de forma descriptiva
+
+## Gráficos modelo "logit"
+
+## Ingresar datos para generar predicción
