9835feb7130e6c3603bffb460ee4ab02baa7f6ee
sydmizar <irenebenigna@gmail.com>
2021-01-26 14:09:03 -0600

    Update main.R

--- Project/main.R
+++ Project/main.R
@@ -27,9 +27,64 @@ allCovidData <- covidData.DB$find('{}')
 ## Heatmap correlaciones atributo vs atributo
 
 #### Análisis ####
-## Modelo "lineal" ENUE para predecir: probabilidad desempleo ~ c(sexo, edad, nivel educativo, sector económico, estado de residencia)
+## Modelo "lineal" ENOE para predecir: probabilidad desempleo ~ c(sexo, edad, nivel educativo, sector económico, estado de residencia)
 
-## Modelo "Logit" ENUE para predecir: probabilidad desempleo ~ c(sexo, edad, nivel educativo, sector económico, estado de residencia)
+## Modelo "Logit" ENOE para predecir: probabilidad desempleo ~ c(sexo, edad, nivel educativo, sector económico, estado de residencia)
+## check results
+summary(allLogitData)
+sapply(allLogitData, sd)
+xtabs(~eda + clase2, data = allLogitData)
+
+allLogitData$clase2 <- factor(allLogitData$clase2)
+mylogit <- glm(clase2 ~ sex + eda + niv_ins + rama, data = allLogitData, family = "binomial")
+summary(mylogit)
+# Call:
+#   glm(formula = clase2 ~ sex + eda + niv_ins + rama, family = "binomial", 
+#       data = allLogitData)
+# 
+# Deviance Residuals: 
+#   Min      1Q  Median      3Q     Max  
+# -8.490   0.000   0.000   0.000   1.392  
+# 
+# Coefficients:
+#   Estimate Std. Error  z value Pr(>|z|)    
+# (Intercept) -15.02512    0.11163 -134.600   <2e-16 ***
+#   sex          -0.03882    0.02620   -1.481    0.138    
+# eda           0.97283    0.01019   95.492   <2e-16 ***
+#   niv_ins       2.93707    0.03676   79.891   <2e-16 ***
+#   rama         13.73648  129.67436    0.106    0.916    
+# ---
+#   Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+# 
+# (Dispersion parameter for binomial family taken to be 1)
+# 
+# Null deviance: 660434  on 689029  degrees of freedom
+# Residual deviance:  37047  on 689025  degrees of freedom
+# (13155 observations deleted due to missingness)
+# AIC: 37057
+# 
+# Number of Fisher Scoring iterations: 25
+
+confint.default(mylogit)
+# 2.5 %      97.5 %
+#   (Intercept)  -15.24391042 -14.8063378
+# sex           -0.09017607   0.0125373
+# eda            0.95285923   0.9927936
+# niv_ins        2.86501159   3.0091220
+# rama        -240.42059136 267.8935560
+wald.test(b = coef(mylogit), Sigma = vcov(mylogit), Terms = 1:4)
+# Wald test:
+#   ----------
+#   
+#   Chi-squared test:
+#   X2 = 29022.7, df = 4, P(> X2) = 0.0
+
+exp(coef(mylogit))
+
+##### Status: In process. Comment: Check other ways (should be 70% train 30% test)
+newdata1 <- with(data_rl, data.frame(sex = mean(sex), eda = mean(eda), niv_ins = mean(niv_ins), rama = mean(rama), clase2 = factor(0:4)))
+newdata1$rankP <- predict(mylogit, newdata = newdata1, type = "response")
+newdata1
 
 # Primer trimestre (sin restricciones)
 # Segundo y tercer trimestre (con restricciones por COVID)
