bd5bcf5c99aae7d23be86704f8ef81b3e401d5eb
Walt9819 <walter22r@icloud.com>
2021-05-20 13:37:18 -0500

    Reorder classes and model performance evaluations

--- Machine_Learning/RestaurantsDNN.py
+++ Machine_Learning/RestaurantsDNN.py
@@ -7,15 +7,16 @@ from sklearn.model_selection import train_test_split
 class RestaurantsDNN(nn.Module):
     def __init__(self, inputNeurons):
         super().__init__()
+        self.relu = nn.ReLU()
         self.inputNeurons = inputNeurons
         self.input = nn.Linear(inputNeurons, 15)
         self.f1 = nn.Linear(15, 10)
+        self.tanh = nn.Tanh()
+        self.dropout = nn.Dropout(0.25) # dropput layer with 0.25 prob
         self.f2 = nn.Linear(10, 5)
         self.output = nn.Linear(5, 1)
         self.sigmoid = nn.Sigmoid()
-        self.tanh = nn.Tanh()
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.25) # dropput layer with 0.25 prob
+
 
     def forward(self, x):
         x = self.tanh(self.input(x))
@@ -26,17 +27,46 @@ class RestaurantsDNN(nn.Module):
         x = self.output(x)
         return self.sigmoid(x) # probability activation layer (softmax like)
 
-    def __str__(self):
-        return f"Restaurants DNN with {self.inputNeurons} input neurons"
 
 
-
-class RestaurantsModel():
+class MainModel():
     def __init__(self, dataset=None, split_at=0.3, random_state=None, **kwargs):
+        """
+        Main restaurants model class init method
+        -------------------------------------------------------------------------------------
+        Params:
+        Name                    |Type               |Description
+
+        dataset(optional)       |Dict               |Dictionary with `x` and `y` keys for
+                                                    |input and label data of iterable type.
+        split_at(default=0.3)   |float              |Size of test set where to split data.
+        random_state(optional)  |int                |Random state for reproducibility.
+        x(optional)             |iterable           |**Only used if `dataset` not given**.
+                                                    |Input data.
+        y(optional)             |iterable           |**Only used if `dataset` not given**.
+                                                    |Labels data.
+        optimizer(default=adam) |torch.optim/list   |Torch optimizer or optimizers to use
+                                                    |while training. If multiple given,
+                                                    |one training will be made for each.
+        lr(default=0.0.1)       |float/list         |Learning rate (η) used on optimizer
+                                                    |update step algorithm. If multiple
+                                                    |given, one training will be done
+                                                    |for each on each optimizer given.
+        scheduler(optional)     |torch.optim.       |Learning rate scheduler used while
+                                |lr_scheduler       |training model.
+        -------------------------------------------------------------------------------------
+        Output:
+        Name                    |Type               |Description
+
+        models                  |torch.nn.Module    |List with each trained model for given
+                                                    |training parameters.
+        -------------------------------------------------------------------------------------
+        """
         # Get x and y from given params
         self.split_at = split_at
         self.random_state = random_state
         self.xtrn = None; self.xtst = None; self.ytrn = None; xelf.ytst = None;
+        # Dataset validation
         if not dataset:
             try:
                 x = kwargs["x"]
@@ -46,14 +76,34 @@ class RestaurantsModel():
         else:
             x = dataset["x"]
             y = dataset["y"]
-
-        self.splitData()
-
-
-    def splitData(self, x, y):
+        # Split validation
         if self.split_at > 1:
             raise ValueError("split_at must be a number between 0 and 1")
 
+        # Get if k fold cross validations is required
+        try:
+            k = kwargs["k_folds"]
+        except KeyError:
+            k = None
+            pass
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+
+        self.splitData() # split data
+
+
+    def _splitData(self):
+        """
+        Split given data into train and test set (**read only**) method.
+        -------------------------------------------------------------------------------------
+        Params:
+        Name                    |Type               |Description
+        -------------------------------------------------------------------------------------
+        Output:
+        Name                    |Type               |Description
+        -------------------------------------------------------------------------------------
+        """
         # Split datasets into train and test
         xTrn, xTst, yTrn, yTst = train_test_split(x, y, test_size=split_at, random_state=random_state)
 
@@ -62,21 +112,73 @@ class RestaurantsModel():
         self.ytrn = torch.tensor(y_train).type(torch.FloatTensor); self.ytst = torch.tensor(y_test).type(torch.FloatTensor);
 
 
+class ModelPerformance():
+    def __init__(self, model, categories=2):
+        """
+        Evaluate model performance for given data
+        -------------------------------------------------------------------------------------
+        Params:
+        Name                    |Type               |Description
 
-class RestaurantsTraining():
-    def __init__(self, model, epochs=30, η=0.01, optimizer=optim.SGD(), lossFunction=nn.BCELoss, k_folds=False, categories=2):
-        self.epochs = epochs
-        self.η = η
-        self.optim = optimizer
-        self.lossFunction = lossFunction
-        self.k_folds = k_folds
+        model                   |torch.nn.Module    |Model to be tested.
+        categories(default=2)   |iterable           |Number of categories to be predicted.
+        -------------------------------------------------------------------------------------
+        Output:
+        Name                    |Type               |Description
+        -------------------------------------------------------------------------------------
+        """
         self.model = model
         self.categories = categories
 
 
-    def performAccuracy(self, y, pred):
-        acc = sum(torch.round(pred * categories) / self.categories == y) # perform accuracy
-        return acc
+    def modelEvaluation(self, x, y):
+        """
+        Evaluate model performance for given data
+        -------------------------------------------------------------------------------------
+        Params:
+        Name                    |Type               |Description
+
+        x                       |iterable           |Input data.
+        y                       |iterable           |Labels data.
+        -------------------------------------------------------------------------------------
+        Output:
+        Name                    |Type               |Description
+
+        evals                   |Dict               |Dictionary with keys for each available
+                                                    |performance test ("acc": accuracy), if
+                                                    |`categories` is equal to 2, more tests given
+                                                    |("sens": sensitivity, "spec": specificity).
+        -------------------------------------------------------------------------------------
+        """
+        if x.size()[0] != y.size()[0]:
+            raise ValueError(f"Mismatched dimensions: input dimension is {x.size()[0]} and labels input is {y.size()[0]}")
+        evals = Dict()
+        with torch.no_grads():
+            sizeY = y.size()[0]
+            pred = self.model(x) # predictions
+            pred = pred.view(pred.size()[0]) # Reshape predictions
+            acc = sum(torch.round(pred * self.categories) / self.categories == y) # perform accuracy
+            evals["acc"] = acc / sizeY
+            if self.categories == 2:
+                TPRes = torch.round(pred) * y
+                TP = sum(TPRes) # ∑ (yŷ)
+                FP = abs(sum(TPRes - pred)) # |∑ (yŷ - ̂y)|
+                FN = abs(sum(TPRes - y)) # |∑ (yŷ - y)|
+                TN = sizeY - FP + FN + TP # sum(y + ̂y - TP) - sizeY
+                evals["sens"] = TP / (TP + TN)
+                evals["spec"] = TN / (TN + FP)
+        return evals
+
+
+class ModelTraining():
+    def __init__(self, model, optimizer=None, lossFunction=None, epochs=30, η=0.01, k_folds=False, categories=2):
+        self.epochs = epochs
+        self.η = η
+        self.optim = optim.SGD()
+        self.lossFunction = nn.BCELoss()
+        self.k_folds = k_folds
+        self.model = model
+        self.categories = categories
 
 
     def trainModel(self, x, y):
@@ -88,16 +190,13 @@ class RestaurantsTraining():
             loss = self.lossFunction(pred, x)
             loss.backward() # Backpropagate
             self.optim.step() # Update params
-            acc = self.performAccuracy(pred, y)
+            acc = model.modelAccuracy(y, pred)
             #print(f"epoch: {epoch} loss: {loss} Acc: {acc}"
-        print("Training performance:")
-        self.testModel(x, y)
+        performance = ModelPerformance(self.model, self.categories)
+        print(f"Trained net performance is:\n{performance}")
+        return self.model
 
 
-    def testModel(self, x, y):
-        with torch.no_grad():
-            testPred = self.model(x)
-            testPred = testPred.view(testPred.size()[0])
-            loss = self.lossFunction(pred, objetivoTrain)
-            acc = performAccuracy(pred, y)
-            print(f"epoch: {epoch} loss: {loss} Acc: {acc}")
+    def modelAccuracy(self, y, pred):
+        acc = sum(torch.round(pred * self.categories) / self.categories == y) # perform accuracy
+        return acc /= y.size()[0]
