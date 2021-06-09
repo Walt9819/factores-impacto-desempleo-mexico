ebf6d9529698663324b1ebe086860e35e0069cc8
EnriqueARguez <76457845+EnriqueARguez@users.noreply.github.com>
2021-06-09 18:55:51 -0500

    Update app.R

--- Machine_Learning/dashboard_bedu/app.R
+++ Machine_Learning/dashboard_bedu/app.R
@@ -1,6 +1,7 @@
 ## app.R ##
 
 library(shiny)
+library(shinyjs)
 library(shinydashboard)
 library(shinythemes)
 library(dplyr)
@@ -9,6 +10,8 @@ library(ggplot2)
 library(plotly)
 
 ui <- fluidPage(
+  
+  useShinyjs(),
   
   dashboardPage(
 #===============================dashboardPage=========================
@@ -51,15 +54,16 @@ ui <- fluidPage(
                 fluidRow(
                   box(
                     titlePanel(h3("Histograma")),
-                    plotOutput("histograma", height = 800),
-                    height = 900
+                    plotOutput("histograma", height = 900),
+                    height = 1050
                   ),
                   
                   box(
                     titlePanel(h3("Gráfica")),
                     selectInput(inputId = "opciones",label = "Seleccionar Opción",choices = c("Ingreso Laboral"=1,"Población Total"=2,"Restaurantes"=3,"Indice de delitos"),selected = "1"),
+                    disabled(selectInput(inputId = "opciones2",label = "Seleccionar Opción",choices = c("Comida a la Carta y Corrida"=1,"Antojitos"=2,"Pescados y Mariscos"=3,"Comida Rapida"=4,"Tacos y Tortas"=5,"Autoservicio"=6,"Otros"=7))),
                     plotOutput("graf1", height = 800),
-                    height = 900
+                    height = 1050
                   )
                   
                 )
@@ -100,23 +104,84 @@ server <- function(input, output) {
     d <- melt(df)
     ggplot(d,aes(x = value)) + 
       facet_wrap(~variable,scales = "free_x", ncol=4) + 
-      geom_histogram(bins=10)
+      geom_histogram(bins=10)+
+      labs(x = "",y = "")
     
   })
   
   output$graf1 <- renderPlot({
     
+    if(input$opciones == 3){
+      enable("opciones2")
+    }
+    else{
+      disable("opciones2")
+    }
+    
     if(input$opciones == 1){
       ggplot(data=df, aes(x=nom_ent, y=ing_lab, group=1)) +
         geom_line(linetype = "dashed", color="blue", size=1)+
         geom_point(color="blue", size=5)+
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+        labs(x = "Entidades",y = "Ingreso Laboral")+
+        theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
     }
     else if(input$opciones == 2){
       ggplot(data=df, aes(x=nom_ent, y=pobtot, group=1)) +
         geom_line(linetype = "dashed", color="blue", size=1)+
         geom_point(color="blue", size=5)+
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+        labs(x = "Entidades",y = "Población Total")+
+        theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    }
+    else if(input$opciones == 3){
+      
+      if(input$opciones2 == 1){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = comida_carta_corrida), color = "red", size=1)+
+          labs(x = "Entidades",y = "Número de restaurantes")+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
+      else if(input$opciones2 == 2){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = antojitos), color="blue", size=1)+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
+      else if(input$opciones2 == 3){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = pescados_mariscos), color = "green", size=1)+
+          labs(x = "Entidades",y = "Número de restaurantes")+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
+      else if(input$opciones2 == 4){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = comida_rapida), color="purple", size=1)+
+          labs(x = "Entidades",y = "Número de restaurantes")+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
+      else if(input$opciones2 == 5){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = tacos_tortas), color = "orange4", size=1)+
+          labs(x = "Entidades",y = "Número de restaurantes")+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
+      else if(input$opciones2 == 6){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = autoservicio), color="deeppink4", size=1)+
+          labs(x = "Entidades",y = "Número de restaurantes")+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
+      else if(input$opciones2 == 7){
+        ggplot(data=df, aes(x=nom_ent, group=1))+
+          geom_line(aes(y = otro_tipo_alimentos), color="goldenrod1", size=1)+
+          labs(x = "Entidades",y = "Número de restaurantes")+
+          coord_cartesian(ylim = c(0, 15000))+
+          theme(text = element_text(size=16),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      }
     }
     
   })
